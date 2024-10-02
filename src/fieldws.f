55d97518002a6e8de5c8015de2228b7b110452c1
jcwright77 <jcwright@mit.edu>
2024-10-02 17:58:27 -0400

    contour plots fix

--- src/fieldws.f
+++ src/fieldws.f
@@ -5526,11 +5526,12 @@ c Call plotter once for f < 0 (dashed), once for f > 0 (solid lines).
 
       if(nlevlt .gt. 0) then
          
-         call pgsci(nyellow)
+         call pgsci(nmagenta)
          call pgcont(f, nrmax, nthmax, 1, nr, 1, nth, flevel(ilevlt),
-     &       nlevlt, tr)
+     &        nlevlt, tr)
+!         call pgconl(f, nrmax, nthmax, 1, nr, 1, nth, flevel(ilevlt),
+!              nlevlt, tr)
       endif
-! C /3.0, 3.2, 3.5, 3.6, 3.766413, 4.0 ,5.0, 10.0, 100.0/
 
       if(nlevgt .gt. 0) then
          CALL PGSFS(1)
@@ -6972,6 +6973,7 @@ c
       call pgmtxt('b', 3.2, 0.5, 0.5, titlb)
       call pgmtxt('t', 2.0, 0.5, 0.5, title)
       call pgmtxt('l', 2.0, 0.5, 0.5, titll)
+      call pgmtxt('r', 2.0, 0.5, 0.5, titlr)
 
       CALL PGSCI(nblue)
       call pgline(nr, x1, y1)
@@ -8233,7 +8235,10 @@ c--Advance graphics frame and get ready to plot
       call pgenv(xmin, xmax, ymin, ymax, 1, 0)
 
       call pgscir(1, nlevel)
+      call palett(2, 1., 0.5)
       call pgimag(f, nrmax, nthmax, 1, nr, 1, nth, fmin, fmax, tr)
+      CALL PGWEDG('BI', 4.0, 5.0, FMIN, FMAX, 'Values')
+
 
       call pgsci(nblack)
       call pglab(titx, tity, title)
@@ -8704,3 +8709,65 @@ C
       RETURN
       END
 
+
+!--------------------------------------------------------------------------------
+      SUBROUTINE PALETT(TYPE, CONTRA, BRIGHT)
+C-----------------------------------------------------------------------
+C Set a "palette" of colors in the range of color indices used by
+C PGIMAG.
+C-----------------------------------------------------------------------
+      INTEGER TYPE
+      REAL CONTRA, BRIGHT
+C
+      REAL GL(2), GR(2), GG(2), GB(2)
+      REAL RL(9), RR(9), RG(9), RB(9)
+      REAL HL(5), HR(5), HG(5), HB(5)
+      REAL WL(10), WR(10), WG(10), WB(10)
+      REAL AL(20), AR(20), AG(20), AB(20)
+C
+      DATA GL /0.0, 1.0/
+      DATA GR /0.0, 1.0/
+      DATA GG /0.0, 1.0/
+      DATA GB /0.0, 1.0/
+C
+      DATA RL /-0.5, 0.0, 0.17, 0.33, 0.50, 0.67, 0.83, 1.0, 1.7/
+      DATA RR / 0.0, 0.0,  0.0,  0.0,  0.6,  1.0,  1.0, 1.0, 1.0/
+      DATA RG / 0.0, 0.0,  0.0,  1.0,  1.0,  1.0,  0.6, 0.0, 1.0/
+      DATA RB / 0.0, 0.3,  0.8,  1.0,  0.3,  0.0,  0.0, 0.0, 1.0/
+C
+      DATA HL /0.0, 0.2, 0.4, 0.6, 1.0/
+      DATA HR /0.0, 0.5, 1.0, 1.0, 1.0/
+      DATA HG /0.0, 0.0, 0.5, 1.0, 1.0/
+      DATA HB /0.0, 0.0, 0.0, 0.3, 1.0/
+C
+      DATA WL /0.0, 0.5, 0.5, 0.7, 0.7, 0.85, 0.85, 0.95, 0.95, 1.0/
+      DATA WR /0.0, 1.0, 0.0, 0.0, 0.3,  0.8,  0.3,  1.0,  1.0, 1.0/
+      DATA WG /0.0, 0.5, 0.4, 1.0, 0.0,  0.0,  0.2,  0.7,  1.0, 1.0/
+      DATA WB /0.0, 0.0, 0.0, 0.0, 0.4,  1.0,  0.0,  0.0, 0.95, 1.0/
+C
+      DATA AL /0.0, 0.1, 0.1, 0.2, 0.2, 0.3, 0.3, 0.4, 0.4, 0.5,
+     :         0.5, 0.6, 0.6, 0.7, 0.7, 0.8, 0.8, 0.9, 0.9, 1.0/
+      DATA AR /0.0, 0.0, 0.3, 0.3, 0.5, 0.5, 0.0, 0.0, 0.0, 0.0,
+     :         0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0/
+      DATA AG /0.0, 0.0, 0.3, 0.3, 0.0, 0.0, 0.0, 0.0, 0.8, 0.8,
+     :         0.6, 0.6, 1.0, 1.0, 1.0, 1.0, 0.8, 0.8, 0.0, 0.0/
+      DATA AB /0.0, 0.0, 0.3, 0.3, 0.7, 0.7, 0.7, 0.7, 0.9, 0.9,
+     :         0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0/
+C
+      IF (TYPE.EQ.1) THEN
+C        -- gray scale
+         CALL PGCTAB(GL, GR, GG, GB, 2, CONTRA, BRIGHT)
+      ELSE IF (TYPE.EQ.2) THEN
+C        -- rainbow
+         CALL PGCTAB(RL, RR, RG, RB, 9, CONTRA, BRIGHT)
+      ELSE IF (TYPE.EQ.3) THEN
+C        -- heat
+         CALL PGCTAB(HL, HR, HG, HB, 5, CONTRA, BRIGHT)
+      ELSE IF (TYPE.EQ.4) THEN
+C        -- weird IRAF
+         CALL PGCTAB(WL, WR, WG, WB, 10, CONTRA, BRIGHT)
+      ELSE IF (TYPE.EQ.5) THEN
+C        -- AIPS
+         CALL PGCTAB(AL, AR, AG, AB, 20, CONTRA, BRIGHT)
+      END IF
+      END
