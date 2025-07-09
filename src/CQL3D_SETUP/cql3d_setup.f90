4e5e94c4831df78a7c957317ce9ad42c58d23081
jacob_van_de_lindt <vandelij@mit.edu>
2025-07-09 09:58:56 -0700

    changed some bugs

--- src/CQL3D_SETUP/cql3d_setup.f90
+++ src/CQL3D_SETUP/cql3d_setup.f90
@@ -23,9 +23,9 @@
       integer, parameter :: n_theta_dim = 300
       integer, parameter :: n_psi_dim = 64
       
-      ! real, allocatable :: R_rzvv(:), Z_rzvv(:)   ! TODO delete this section 
-      ! real, allocatable :: vPer_rzvv(:), vPar_rzvv(:)
-      ! real, allocatable :: f_rzvv(:,:,:,:)
+      real, dimension(:), allocatable :: R_rzvv, Z_rzvv   ! TODO delete this section 
+      real, dimension(:), allocatable :: vPer_rzvv, vPar_rzvv
+      real, dimension(:,:,:,:), allocatable :: f_rzvv
 !     ----------------
 !     450 x 450 modes:
 !     ----------------
@@ -253,18 +253,19 @@
 
       ! Temporary test TODO: delete this section, just for testing distribution read-in.
 
-   !    call netcdfr4d_single_gen(f4d_file1, R_rzvv, Z_rzvv,
-   !   &      vPer_rzvv, vPar_rzvv, f_rzvv)
+      call netcdfr4d_single_gen(f4d_file1, R_rzvv, Z_rzvv,
+     &      vPer_rzvv, vPar_rzvv, f_rzvv)
 
-   !    print *, "Loaded f_rzvv with shape: ", 
-   !   &      size(f_rzvv,1), size(f_rzvv,2), 
-   !   &      size(f_rzvv,3), size(f_rzvv,4)
+      print *, "Loaded f_rzvv with shape: ", 
+     &      size(f_rzvv,1), size(f_rzvv,2), 
+     &      size(f_rzvv,3), size(f_rzvv,4)
 
-   !          call blacs_barrier(icontxt, 'All')
-
-   !          open(unit=50, file='cql3d1.out', status='unknown',
-   !   &                                                 form='formatted')
 !     end read in test delete me 
+      call blacs_barrier(icontxt, 'All')
+
+      open(unit=50, file='cql3d1.out', status='unknown',
+     &                                                 form='formatted')
+
 
       kspeci = 1 !reversed from normal order for reasons... 
       !JVDL: switched this ^ back to 1, looks like SF had order reversed in cql?
