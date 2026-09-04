54c639cb65c31d2550e9e58d8ec6f7fe0b2b431b
jcwright77 <jcwright@mit.edu>
2026-09-03 21:25:35 -0400

    Fixing tests after new github images breaks relative paths

--- test/DIIID-helicon/cleanrun.sh
+++ test/DIIID-helicon/cleanrun.sh
@@ -1,10 +1,10 @@
-rm mchoi*
-rm movie*
-rm AORSA2D*
-rm bertelli *.vtk *.ps rho out* zowens umat swain naoto* murakami inone fpm* E_lab_frame
-rm Ono_disp fields_* namelist_input
-rm *.txt
-rm log_*
-rm bharvey3_3d bharvey_3d
-rm bharvey*
-rm logfile
+rm -f mchoi*
+rm -f movie*
+rm -f AORSA2D*
+rm -f bertelli *.vtk *.ps rho out* zowens umat swain naoto* murakami inone fpm* E_lab_frame
+rm -f Ono_disp fields_* namelist_input
+rm -f *.txt
+rm -f log_*
+rm -f bharvey3_3d bharvey_3d
+rm -f bharvey*
+rm -f logfile
